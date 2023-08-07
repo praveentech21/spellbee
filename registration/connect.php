be841848db64c1320b9e169159897aaf7456e2cc
Sai Praveen <ravikumar_csd@srkrec.edu.in>
2023-08-07 22:41:55 +0530

    Some more Unwantedes Shiva

--- registration/connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$conn=mysqli_connect('localhost','codemaster2018','codemaster2018@6464', 'codemaster2018');
-
-?>
--- registration/connect2.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-mysql_connect('localhost','codemaster2016','codemaster2016@8080')  || die ("To Be Updated Soon!");
-mysql_select_db('codemaster2016') OR die("Iam unableto select a database");
-
-?>
