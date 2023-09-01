a929bc0c96be6e23e9a18a749499de0637507301
Sai Praveen <ravikumar_csd@srkrec.edu.in>
2023-09-01 16:54:05 +0530

    fotter team setted shiva

--- footer.php
+++ footer.php
@@ -1,84 +1,153 @@
 <!-- BEGIN PRE-FOOTER -->
 <div class="pre-footer" id="contact">
-        <div class="container">
-            <div class="row">
-                <!-- BEGIN BOTTOM ABOUT BLOCK -->
-                <div class="col-md-4 col-sm-6 pre-footer-col">
-                    <h2>About us</h2>
-                    <p style="align-self:auto;">Spell Champ is jointly organized by Campus Online, English Department and Idea Lab of SRKR Engineering College, Bhimavaram. This Event is Started in this year and expeted to Countinue futher years </p>
-                </div>
-                <!-- END BOTTOM ABOUT BLOCK -->
-                <!-- BEGIN TWITTER BLOCK -->
-                <div class="col-md-4 col-sm-6 pre-footer-col">
-                    <h2 class="margin-bottom-0">CHECK OUT </h2>
-                    <!-- <h5><a href='http://saipraveen.free.nf/campusonline' target='_new'>Campus Online</a></h5> -->
-                    <h5><a href='register.php' target='_new'>Register</a></h5>
-                    <!-- <h5><a href='ranks.php' target='_new'>Ranks</a></h5> -->
-                    <h5><a href='rules.php' target='_new'>Rules</a></h5>
-                    <!-- <h5><a href='certificate.php' target='_new'>Certificate</a></h5> -->
-                    <!-- <h5><a href='prizes.php' target='_new'>Prizes</a></h5> -->
-                    <h5><a href='feedback.php' target='_new'>Feedback</a></h5>
-                </div>
-                <!-- END TWITTER BLOCK -->
-                <div class="col-md-4 col-sm-6 pre-footer-col">
-                    <!-- BEGIN BOTTOM CONTACTS -->
-                    <h2>Our Contacts</h2>
-                    <address class="margin-bottom-20">
-                        MCR Web Solutions & Technology Centre<br>
-                        2nd Floor, I-Hub Incubation Centre,<br>
-                        Idea Lab, SRKR Engineering College,<br>
-                        Chinna Amiram, Bhimavaram, A.P. - 534204<br>
-                        Phone: <a href="tel:+9848823311">+91 98488 23311</a> <a href="tel:+9052727402">+91 90527 27402</a><br>
-                        <!-- Email: <a href="mailto:spellbee2k23@srkrec.edu.in">spellbee2k23@srkrec.edu.in</a><br> -->
-                    </address>
-                    <!-- END BOTTOM CONTACTS -->
-                </div>
+    <div class="container">
+        <div class="row">
+            <!-- BEGIN BOTTOM ABOUT BLOCK -->
+            <div class="col-md-3 col-sm-6 pre-footer-col">
+                <h2>About us</h2>
+                <p style="align-self:auto;">SRKR SpellBee Contest is jointly organized by Computer Science & Design Branch in association with English Department and AICTE IdeaLab of SRKR Engineering College, Bhimavaram. The contest is conducted completely online using the custom software developed by the Software Development Club (SDC) of Computer Science & Design branch. </p>
+            </div>
+            <!-- END BOTTOM ABOUT BLOCK -->
+            <!-- BEGIN TWITTER BLOCK -->
+            <div class="col-md-3 col-sm-6 pre-footer-col">
+                <h2 class="margin-bottom-0">CHECK OUTS </h2>
+                <!-- <h5><a href='http://saipraveen.free.nf/campusonline' target='_new'>Campus Online</a></h5> -->
+                <h5><a href='register.php' target='_new'>Register</a></h5>
+                <!-- <h5><a href='ranks.php' target='_new'>Ranks</a></h5> -->
+                <h5><a href='rules.php' target='_new'>Rules</a></h5>
+                <!-- <h5><a href='certificate.php' target='_new'>Certificate</a></h5> -->
+                <!-- <h5><a href='prizes.php' target='_new'>Prizes</a></h5> -->
+                <h5><a href='feedback.php' target='_new'>Feedback</a></h5>
+            </div>
+
+            <div class="col-md-3 col-sm-6 pre-footer-col">
+                <h2 class="margin-bottom-0">TEAM </h2>
+                <table style="border-collapse: collapse; margin: 0; padding: 0;">
+                    <tr style="height: 50px;" >
+                        <td style="padding: 0; margin: 0;">
+                            <div class="mt-2" style="margin: 0;">
+                                <p class="powered" style="margin: 0;"> <a href="" target='_new'><img src='assets/onepage/img/team/1.png' style="width: 100px;  margin: 0;"></a></p>
+                            </div>
+                        </td>
+                        <td style="padding: 0; margin: 0;">
+                            <div class="mt-2" style="margin: 0;">
+                                <p class="powered" style="margin: 0;"> <a href="" target='_new'><img src='assets/onepage/img/team/2.png' style="width: 100px;  margin: 0;"></a></p>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr style="height: 50px;">
+                        <td style="padding: 0; margin: 0;">
+                            <div class="mt-2" style="margin: 0;">
+                                <p class="powered" style="margin: 0;"> <a href="" target='_new'><img src='assets/onepage/img/team/5.png' style="width: 100px;  margin: 0;"></a></p>
+                            </div>
+                        </td>
+                        <td style="padding: 0; margin: 0;">
+                            <div class="mt-2" style="margin: 0;">
+                                <p class="powered" style="margin: 0;"> <a href="" target='_new'><img src='assets/onepage/img/team/6.png' style="width: 100px;  margin: 0;"></a></p>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr style="height: 50px;">
+                        <td style="padding: 0; margin: 0;">
+                            <div class="mt-2" style="margin: 0;">
+                                <p class="powered" style="margin: 0;"> <a href="" target='_new'><img src='assets/onepage/img/team/4.png' style="width: 100px;  margin: 0;"></a></p>
+                            </div>
+                        </td>
+                        <td style="padding: 0; margin: 0;">
+                            <div class="mt-2" style="margin: 0;">
+                                <p class="powered" style="margin: 0;"> <a href="" target='_new'><img src='assets/onepage/img/team/3.png' style="width: 100px;  margin: 0;"></a></p>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr style="height: 50px;">
+                        <td style="padding: 0; margin: 0;">
+                            <div class="mt-2" style="margin: 0;">
+                                <p class="powered" style="margin: 0;"> <a href="" target='_new'><img src='assets/onepage/img/team/7.png' style="width: 100px;  margin: 0;"></a></p>
+                            </div>
+                        </td>
+                        <td style="padding: 0; margin: 0;">
+                            <div class="mt-2" style="margin: 0;">
+                                <p class="powered" style="margin: 0;"> <a href="" target='_new'><img src='assets/onepage/img/team/8.png' style="width: 100px;  margin: 0;"></a></p>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr style="height: 50px;">
+                        <td style="padding: 0; margin: 0;">
+                            <div class="mt-2" style="margin: 0;">
+                                <p class="powered" style="margin: 0;"> <a href="" target='_new'><img src='assets/onepage/img/team/10.png' style="width: 100px;  margin: 0;"></a></p>
+                            </div>
+                        </td>
+                        <td style="padding: 0; margin: 0;">
+                            <div class="mt-2" style="margin: 0;">
+                                <p class="powered" style="margin: 0;"> <a href="" target='_new'><img src='assets/onepage/img/team/9.png' style="width: 100px;  margin: 0;"></a></p>
+                            </div>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+
+
+
+            <!-- END TWITTER BLOCK -->
+            <div class="col-md-3 col-sm-6 pre-footer-col"6>
+                <!-- BEGIN BOTTOM CONTACTS -->
+                <h2>Our Contacts</h2>
+                <address class="margin-bottom-20">
+                    Computer Science & Design (CSD) Branch<br>
+                    2nd Floor, I-Hub Incubation Centre,<br>
+                    Idea Lab, SRKR Engineering College,<br>
+                    Chinna Amiram, Bhimavaram, A.P. - 534204<br>
+                    Phone : K.Sanju <a href="tel:+9848823311">+91 98488 23311</a><br>Ch. Ravi Kumar <a href="tel:+9052727402">+91 90527 27402</a><br>
+                    <!-- Email: <a href="mailto:spellbee2k23@srkrec.edu.in">spellbee2k23@srkrec.edu.in</a><br> -->
+                </address>
+                <!-- END BOTTOM CONTACTS -->
             </div>
         </div>
     </div>
-    <!-- END PRE-FOOTER -->
+</div>
+<!-- END PRE-FOOTER -->
 
-    <!-- BEGIN FOOTER -->
-    <div class="footer">
-        <div class="container">
-            <div class="row">
-                <!-- BEGIN COPYRIGHT -->
-                <div class="col-md-8 col-sm-8">
-                    <div class="copyright">© Code Master Contest 2018 . All Rights Reserved.</div>
-                </div>
-                <!-- END COPYRIGHT -->
-                <!-- BEGIN COPYRIGHT -->
-                <div class="col-md-2 col-sm-2 text-right">
-                    <p class="powered">Powered by: <a href="http://www.mcr.org.in/" target='_new'><img src='assets/onepage/img/mcr.png'></a></p>
-                </div>
-                <!-- END COPYRIGHT -->
-                <!-- BEGIN SOCIAL ICONS -->
-                <div class="col-md-2 col-sm-2 text-left">
-                    <ul class="social-icons">
-                        <li><a class="facebook" data-original-title="facebook" href="https://www.facebook.com/mcrweb/" target='_new'></a></li>
-                        <li><a class="twitter" data-original-title="twitter" href="https://twitter.com/mcr_web" target='_new'></a></li>
-                    </ul>
-                </div>
-                <!-- END SOCIAL ICONS -->
-
+<!-- BEGIN FOOTER -->
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <!-- BEGIN COPYRIGHT -->
+            <div class="col-md-8 col-sm-8">
+                <div class="copyright">© Computer Science & Design, SRKREC 2023 . All Rights Reserved.</div>
             </div>
+            <!-- END COPYRIGHT -->
+            <!-- BEGIN COPYRIGHT -->
+            <div class="col-md-2 col-sm-2 text-right">
+                <p class="powered">Powered by: <a  target='_new'><img src='assets/onepage/img/csd.png'></a></p>
+            </div>
+            <!-- END COPYRIGHT -->
+            <!-- BEGIN SOCIAL ICONS -->
+            <div class="col-md-2 col-sm-2 text-left">
+                <ul class="social-icons">
+                    <li><a class="facebook" data-original-title="facebook" href="https://www.facebook.com/mcrweb/" target='_new'></a></li>
+                    <li><a class="twitter" data-original-title="twitter" href="https://twitter.com/mcr_web" target='_new'></a></li>
+                </ul>
+            </div>
+            <!-- END SOCIAL ICONS -->
+
         </div>
     </div>
-    <!-- END FOOTER -->
-    <a href="#promo-block" class="go2top scroll"><i class="fa fa-arrow-up"></i></a>
+</div>
+<!-- END FOOTER -->
+<a href="#promo-block" class="go2top scroll"><i class="fa fa-arrow-up"></i></a>
 
-    <!-- Core plugins BEGIN (For ALL pages) -->
-    <script src="assets/plugins/jquery.min.js" type="text/javascript"></script>
-    <script src="assets/plugins/jquery-migrate.min.js" type="text/javascript"></script>
-    <script src="assets/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-    <!-- Core plugins END (For ALL pages) -->
-    <!-- Core plugins BEGIN (required only for current page) -->
-    <script src="assets/plugins/fancybox/source/jquery.fancybox.pack.js" type="text/javascript"></script><!-- pop up -->
-    <script src="assets/plugins/jquery.easing.js"></script>
-    <script src="assets/plugins/jquery.parallax.js"></script>
-    <script src="assets/plugins/jquery.scrollTo.min.js"></script>
-    <script src="assets/onepage/scripts/jquery.nav.js"></script>
-    <!-- Core plugins END (required only for current page) -->
-    <!-- Global js BEGIN -->
-    <script src="assets/onepage/scripts/layout.js" type="text/javascript"></script>
-    <script src="assets/pages/scripts/bs-carousel.js" type="text/javascript"></script>
+<!-- Core plugins BEGIN (For ALL pages) -->
+<script src="assets/plugins/jquery.min.js" type="text/javascript"></script>
+<script src="assets/plugins/jquery-migrate.min.js" type="text/javascript"></script>
+<script src="assets/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<!-- Core plugins END (For ALL pages) -->
+<!-- Core plugins BEGIN (required only for current page) -->
+<script src="assets/plugins/fancybox/source/jquery.fancybox.pack.js" type="text/javascript"></script><!-- pop up -->
+<script src="assets/plugins/jquery.easing.js"></script>
+<script src="assets/plugins/jquery.parallax.js"></script>
+<script src="assets/plugins/jquery.scrollTo.min.js"></script>
+<script src="assets/onepage/scripts/jquery.nav.js"></script>
+<!-- Core plugins END (required only for current page) -->
+<!-- Global js BEGIN -->
+<script src="assets/onepage/scripts/layout.js" type="text/javascript"></script>
+<script src="assets/pages/scripts/bs-carousel.js" type="text/javascript"></script>
