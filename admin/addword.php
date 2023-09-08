b16c0f1b83e7896b272d882d371acef355b89094
Sai Praveen <ravikumar_csd@srkrec.edu.in>
2023-09-08 08:16:19 +0530

    adding words done shiva

--- admin/addword.php
+++ admin/addword.php
@@ -1,34 +1,12 @@
 <?php
 session_start();
-if (!isset($_SESSION['admin'])) header("location: login.php");
+if (!isset($_SESSION['admin']))
+  header('location: login.php');
 $admin = $_SESSION['admin'];
 
-include "connect.php";
+include 'connect.php';
 
-if(isset($_POST['addnewword'])){
-  // Define the target directory where you want to store the uploaded MP3 files
-  $targetDir = "";
-
-  // Get the original file name provided by the user
-  $fileName = $_FILES["sound"]["name"];
-  $targetFilePath = $targetDir . $fileName;
-
-  // Check if the file type is MP3
-  $fileType = strtolower(pathinfo($targetFilePath, PATHINFO_EXTENSION));
-  if($fileType == "mp3") {
-      // Upload the file to the specified directory
-      if(move_uploaded_file($_FILES["sound"]["tmp_name"], $targetFilePath)) {
-          echo "The file ". $fileName . " has been uploaded.";
-      } else {
-          echo "Sorry, there was an error uploading your file.";
-      }
-  } else {
-      echo "Only MP3 files are allowed.";
-  }
-  
-  // Handle the rest of the form data and database insertion here
-  
-  // Example: Insert the form data into the database
+if (isset($_POST['addnewword'])) {
   $qid = $_POST['qid'];
   $word = $_POST['word'];
   $meaning = $_POST['meaning'];
@@ -36,11 +14,36 @@ if(isset($_POST['addnewword'])){
   $option1 = $_POST['option1'];
   $option2 = $_POST['option2'];
   $option3 = $_POST['option3'];
-  
-  // Perform database insertion using the form data and the uploaded file path
-  // You can add your database insertion code here
-  
-  // After successful database insertion, you can redirect or display a success message
+
+  if (mysqli_num_rows(mysqli_query($conn, "SELECT * FROM `words` WHERE `qid` = '$qid' or `word` = '$word'")) > 0) {
+    echo "<script>alert('Question Already Exists');</script>";
+  } else {
+    // Define the target directory where you want to store the uploaded MP3 files
+    $targetDir = '../exam/sounds/machine/';
+
+    // Get the original file name provided by the user
+    $fileName = $_FILES['sound']['name'];
+    $targetFilePath = $targetDir . $fileName;
+
+    // Check if the file type is MP3
+    $fileType = strtolower(pathinfo($targetFilePath, PATHINFO_EXTENSION));
+    if ($fileType == 'mp3') {
+      // Upload the file to the specified directory
+      if (move_uploaded_file($_FILES['sound']['tmp_name'], $targetFilePath)) {
+        $addnewword = $conn->prepare('INSERT INTO `words`(`qid`, `word`, `meaning`, `level`, `option1`, `option2`, `option3`) VALUES (?,?,?,?,?,?,?)');
+        $addnewword->bind_param('ississs', $qid, $word, $meaning, $level, $option1, $option2, $option3);
+        if ($addnewword->execute()) {
+          echo "<script>alert('Word Added Successfully');</script>";
+        } else {
+          echo "<script>alert('Word Adding Failed');</script>";
+        }
+      } else {
+        echo "<script>alert('Sorry, there was an error uploading your file.');</script>";
+      }
+    } else {
+      echo "<script>alert('Sorry!..  Only MP3 files are allowed. Uplode an mp3 file');</script>";
+    }
+  }
 }
 
 ?>
@@ -89,7 +92,7 @@ if(isset($_POST['addnewword'])){
 
   <!-- Content Starts Here Shiva-->
   <div class="container-xxl flex-grow-1 container-p-y">
-    <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">SPELLBEE</span> REGISTRATIONS</h4>
+    <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">SPELLBEE</span> WORDS</h4>
 
     <div class="row">
 
@@ -98,16 +101,16 @@ if(isset($_POST['addnewword'])){
       <div class="col-xxl">
         <div class="card mb-4">
           <div class="card-header d-flex align-items-center justify-content-between">
-            <h5 class="mb-0">New Student Registration</h5>
+            <h5 class="mb-0">New Word Addition</h5>
             <!-- <small class="text-muted float-end">Default label</small> -->
           </div>
           <div class="card-body">
-            <form method="post" action="#" onsubmit="return validateForm();">
+            <form method="post" action="#" onsubmit="return validateForm();" enctype="multipart/form-data">
               <div class="row mb-3">
                 <label class="col-sm-2 col-form-label" for="qid">Question ID</label>
                 <div class="col-sm-10">
                   <div class="input-group input-group-merge">
-                    <input type="qid" name="qid" id="qid" autocomplete="off" class="form-control" placeholder="Question Id" required />
+                    <input type="text" name="qid" id="qid" autocomplete="off" class="form-control" placeholder="Question Id" required />
                   </div>
                   <div class="form-text">Question Id File name and questionid must be same</div>
                   <div id="qid-error" class="error"></div>
@@ -130,7 +133,7 @@ if(isset($_POST['addnewword'])){
               <div class="row mb-3">
                 <label class="col-sm-2 col-form-label" for="sound">Sound</label>
                 <div class="col-sm-10">
-                  <input class="form-control" type="file" id="sound" name="sound" />
+                  <input class="form-control" type="file" id="sound" name="sound" required />
                 </div>
               </div>
               <div class="row mb-3">
@@ -156,21 +159,21 @@ if(isset($_POST['addnewword'])){
               <div class="row mb-3">
                 <label class="col-sm-2 col-form-label" for="option1">OPTION 1</label>
                 <div class="col-sm-10">
-                  <input type="text" id="option1" autocomplete="off" name="option1" class="form-control phone-mask" placeholder="Options for this words" />
+                  <input type="text" required id="option1" autocomplete="off" name="option1" class="form-control phone-mask" placeholder="Options for this words" />
                 </div>
                 <div id="option1-error" class="error"></div>
               </div>
               <div class="row mb-3">
                 <label class="col-sm-2 col-form-label" for="option2">OPTION 2</label>
                 <div class="col-sm-10">
-                  <input type="text" id="option2" autocomplete="off" name="option2" class="form-control phone-mask" placeholder="Options for this words" />
+                  <input type="text" required id="option2" autocomplete="off" name="option2" class="form-control phone-mask" placeholder="Options for this words" />
                 </div>
                 <div id="option2-error" class="error"></div>
               </div>
               <div class="row mb-3">
                 <label class="col-sm-2 col-form-label" for="option3">OPTION 3</label>
                 <div class="col-sm-10">
-                  <input type="text" id="option3" autocomplete="off" name="option3" class="form-control phone-mask" placeholder="Options for this words" />
+                  <input type="text" required id="option3" autocomplete="off" name="option3" class="form-control phone-mask" placeholder="Options for this words" />
                 </div>
                 <div id="option3-error" class="error"></div>
               </div>
@@ -227,6 +230,11 @@ if(isset($_POST['addnewword'])){
 
       if (qid === "") {
         document.getElementById("qid-error").innerHTML = "qid must be filled out";
+        isValid = false;
+      }
+      // qid must be a number
+      else if (isNaN(qid)) {
+        document.getElementById("qid-error").innerHTML = "qid must be a number";
         isValid = false;
       }
 
--- admin/header.php
+++ admin/header.php
@@ -1,5 +1,6 @@
 <?php
-if ($_SESSION['category'] == 'stall') { ?>
+if ($_SESSION['category'] == 'stall') {
+  ?>
   <div class="layout-wrapper layout-content-navbar">
     <div class="layout-container">
 
@@ -113,6 +114,17 @@ if ($_SESSION['category'] == 'stall') { ?>
 
           </li>
 
+          <li class="menu-item ">
+
+            <a href="addword.php" class="menu-link">
+
+              <i class="menu-icon tf-icons bx bx-table"></i>
+
+              <div data-i18n="Analytics">Add New Words</div>
+
+            </a>
+
+          </li>
 
 
           <li class="menu-item ">
@@ -203,207 +215,220 @@ if ($_SESSION['category'] == 'stall') { ?>
         <div class="content-wrapper">
         <?php } elseif ($_SESSION['category'] == 'superadmin') { ?>
           <div class="layout-wrapper layout-content-navbar">
-    <div class="layout-container">
+            <div class="layout-container">
 
-      <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
-        <div class="app-brand demo">
-          <a href="../index.php" class="app-brand-link">
-            <img src="Bhavani/img/red.png" alt="">
-          </a>
-        </div>
+              <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+                <div class="app-brand demo">
+                  <a href="../index.php" class="app-brand-link">
+                    <img src="Bhavani/img/red.png" alt="">
+                  </a>
+                </div>
 
-        <div class="menu-inner-shadow"></div>
+                <div class="menu-inner-shadow"></div>
 
-        <ul class="menu-inner py-1">
-          <!-- Dashboard -->
-          <!-- Layouts -->
+                <ul class="menu-inner py-1">
+                  <!-- Dashboard -->
+                  <!-- Layouts -->
 
-          <li class="menu-item ">
+                  <li class="menu-item ">
 
-            <a href="index.php" class="menu-link">
+                    <a href="index.php" class="menu-link">
 
-              <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                      <i class="menu-icon tf-icons bx bx-home-circle"></i>
 
-              <div data-i18n="Analytics">Home</div>
+                      <div data-i18n="Analytics">Home</div>
 
-            </a>
+                    </a>
 
-          </li>
+                  </li>
 
-          <li class="menu-item ">
+                  <li class="menu-item ">
 
-            <a href="dashboard.php" class="menu-link">
+                    <a href="dashboard.php" class="menu-link">
 
-              <i class="menu-icon tf-icons bx bx-crown"></i>
+                      <i class="menu-icon tf-icons bx bx-crown"></i>
 
-              <div data-i18n="Analytics">Dashbord</div>
+                      <div data-i18n="Analytics">Dashbord</div>
 
-            </a>
+                    </a>
 
-          </li>
+                  </li>
 
-          <li class="menu-item ">
+                  <li class="menu-item ">
 
-            <a href="student_datails.php" class="menu-link">
+                    <a href="student_datails.php" class="menu-link">
 
-              <i class="menu-icon tf-icons bx bx-lock-open-alt"></i>
+                      <i class="menu-icon tf-icons bx bx-lock-open-alt"></i>
 
-              <div data-i18n="Analytics">Players</div>
+                      <div data-i18n="Analytics">Players</div>
 
-            </a>
+                    </a>
 
-          </li>
+                  </li>
 
-          <li class="menu-item ">
+                  <li class="menu-item ">
 
-            <a href="register.php" class="menu-link">
+                    <a href="register.php" class="menu-link">
 
-              <i class="menu-icon tf-icons bx bx-user"></i>
+                      <i class="menu-icon tf-icons bx bx-user"></i>
 
-              <div data-i18n="Analytics">Registration</div>
+                      <div data-i18n="Analytics">Registration</div>
 
-            </a>
+                    </a>
 
-          </li>
+                  </li>
 
-          <li class="menu-item ">
+                  <li class="menu-item ">
 
-            <a href="payment_conformation.php" class="menu-link">
+                    <a href="payment_conformation.php" class="menu-link">
 
-              <i class="menu-icon tf-icons bx bx-copy"></i>
+                      <i class="menu-icon tf-icons bx bx-copy"></i>
 
-              <div data-i18n="Analytics">Payment Confirmation</div>
+                      <div data-i18n="Analytics">Payment Confirmation</div>
 
-            </a>
+                    </a>
 
-          </li>
+                  </li>
 
-          <li class="menu-item ">
+                  <li class="menu-item ">
 
-            <a href="game_confirmation.php" class="menu-link">
+                    <a href="game_confirmation.php" class="menu-link">
 
-              <i class="menu-icon tf-icons bx bx-support"></i>
+                      <i class="menu-icon tf-icons bx bx-support"></i>
 
-              <div data-i18n="Analytics">Game Confirmation</div>
+                      <div data-i18n="Analytics">Game Confirmation</div>
 
-            </a>
+                    </a>
 
-          </li>
+                  </li>
 
-          <li class="menu-item ">
+                  <li class="menu-item ">
 
-            <a href="editstudent.php" class="menu-link">
+                    <a href="editstudent.php" class="menu-link">
 
-              <i class="menu-icon tf-icons bx bx-copy"></i>
+                      <i class="menu-icon tf-icons bx bx-copy"></i>
 
-              <div data-i18n="Analytics">EDIT USER</div>
+                      <div data-i18n="Analytics">EDIT USER</div>
 
-            </a>
+                    </a>
 
-          </li>
+                  </li>
 
+                  <li class="menu-item ">
 
-          <li class="menu-item ">
+                    <a href="addword.php" class="menu-link">
 
-            <a href="certificate.php" class="menu-link">
+                      <i class="menu-icon tf-icons bx bx-table"></i>
 
-              <i class="menu-icon tf-icons bx bx-file"></i>
+                      <div data-i18n="Analytics">Add New Words</div>
 
-              <div data-i18n="Analytics">Certificate</div>
+                    </a>
 
-            </a>
+                  </li>
 
-          </li>
 
 
+                  <li class="menu-item ">
 
-          <li class="menu-item ">
+                    <a href="certificate.php" class="menu-link">
 
-            <a href="replay.php" class="menu-link">
+                      <i class="menu-icon tf-icons bx bx-file"></i>
 
-              <i class="menu-icon tf-icons bx bx-dock-top"></i>
+                      <div data-i18n="Analytics">Certificate</div>
 
-              <div data-i18n="Analytics">Replay</div>
+                    </a>
 
-            </a>
+                  </li>
 
-          </li>
 
 
-          <li class="menu-item ">
+                  <li class="menu-item ">
 
-            <a href="logout.php" class="menu-link">
+                    <a href="replay.php" class="menu-link">
 
-              <i class="menu-icon tf-icons bx bx-cube-alt"></i>
+                      <i class="menu-icon tf-icons bx bx-dock-top"></i>
 
-              <div data-i18n="Analytics">Logout</div>
+                      <div data-i18n="Analytics">Replay</div>
 
-            </a>
+                    </a>
 
-          </li>
+                  </li>
 
 
-        </ul>
-      </aside>
-      <div class="layout-page">
-        <!-- Navbar -->
+                  <li class="menu-item ">
 
-        <nav class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme" id="layout-navbar">
+                    <a href="logout.php" class="menu-link">
 
-          <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
+                      <i class="menu-icon tf-icons bx bx-cube-alt"></i>
 
-            <div class="navbar-nav align-items-center">
-              <div class="nav-item d-flex align-items-center">
-                <i class="bx bx-search fs-4 lh-0"></i>
-                <input type="text" class="form-control border-0 shadow-none" id="searchInput" title="Search with regno or name or phone number....." placeholder="Search with regno or name or phone number....." aria-label="Search with regno or name or phone number....." />
-              </div>
-            </div>
+                      <div data-i18n="Analytics">Logout</div>
 
-            <ul class="navbar-nav flex-row align-items-center ms-auto">
-              <!-- User -->
-              <li class="nav-item navbar-dropdown dropdown-user dropdown">
-                <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
-                  <div class="avatar avatar-online">
-                    <img src="Bhavani/img/team_1.jpg" alt class="w-px-40 h-auto rounded-circle" />
-                  </div>
-                </a>
-                <ul class="dropdown-menu dropdown-menu-end">
-                  <li>
-                    <a class="dropdown-item" href="#">
-                      <div class="d-flex">
-                        <div class="flex-shrink-0 me-3">
+                    </a>
+
+                  </li>
+
+
+                </ul>
+              </aside>
+              <div class="layout-page">
+                <!-- Navbar -->
+
+                <nav class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme" id="layout-navbar">
+
+                  <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
+
+                    <div class="navbar-nav align-items-center">
+                      <div class="nav-item d-flex align-items-center">
+                        <i class="bx bx-search fs-4 lh-0"></i>
+                        <input type="text" class="form-control border-0 shadow-none" id="searchInput" title="Search with regno or name or phone number....." placeholder="Search with regno or name or phone number....." aria-label="Search with regno or name or phone number....." />
+                      </div>
+                    </div>
+
+                    <ul class="navbar-nav flex-row align-items-center ms-auto">
+                      <!-- User -->
+                      <li class="nav-item navbar-dropdown dropdown-user dropdown">
+                        <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
                           <div class="avatar avatar-online">
                             <img src="Bhavani/img/team_1.jpg" alt class="w-px-40 h-auto rounded-circle" />
                           </div>
-                        </div>
-                        <div class="flex-grow-1">
-                          <span class="fw-semibold d-block">Dr. Suresh Babu</span>
-                          <small class="text-muted">Founder BVRMOL</small>
-                        </div>
-                      </div>
-                    </a>
-                  </li>
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end">
+                          <li>
+                            <a class="dropdown-item" href="#">
+                              <div class="d-flex">
+                                <div class="flex-shrink-0 me-3">
+                                  <div class="avatar avatar-online">
+                                    <img src="Bhavani/img/team_1.jpg" alt class="w-px-40 h-auto rounded-circle" />
+                                  </div>
+                                </div>
+                                <div class="flex-grow-1">
+                                  <span class="fw-semibold d-block">Dr. Suresh Babu</span>
+                                  <small class="text-muted">Founder BVRMOL</small>
+                                </div>
+                              </div>
+                            </a>
+                          </li>
 
-                  <li>
-                    <div class="dropdown-divider"></div>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="logout.php">
-                      <i class="bx bx-power-off me-2"></i>
-                      <span class="align-middle">Log Out</span>
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <!--/ User -->
-            </ul>
-          </div>
-        </nav>
+                          <li>
+                            <div class="dropdown-divider"></div>
+                          </li>
+                          <li>
+                            <a class="dropdown-item" href="logout.php">
+                              <i class="bx bx-power-off me-2"></i>
+                              <span class="align-middle">Log Out</span>
+                            </a>
+                          </li>
+                        </ul>
+                      </li>
+                      <!--/ User -->
+                    </ul>
+                  </div>
+                </nav>
 
-        <!-- / Navbar -->
+                <!-- / Navbar -->
 
-        <div class="content-wrapper">
-                <?php } else {  ?>
+                <div class="content-wrapper">
+                <?php } else { ?>
 
                   <!-- Sidebar Starts Here Shiva -->
                   <div class="layout-wrapper layout-content-navbar">
@@ -497,4 +522,4 @@ if ($_SESSION['category'] == 'stall') { ?>
 
                           <!-- Sidebar Ends Here Shiva -->
 
-                        <?php  } ?>
+                        <?php } ?>
