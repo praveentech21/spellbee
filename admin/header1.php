bd13f9f483825b907ebbf5d5071027edce4ef49e
Sai Praveen <ravikumar_csd@srkrec.edu.in>
2023-09-08 08:30:23 +0530

    heders after update db shiva

---
+++ admin/header1.php
@@ -0,0 +1,525 @@
+<?php
+if ($_SESSION['category'] == 'stall') {
+  ?>
+  <div class="layout-wrapper layout-content-navbar">
+    <div class="layout-container">
+
+      <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+        <div class="app-brand demo">
+          <a href="../index.php" class="app-brand-link">
+            <img src="Bhavani/img/red.png" alt="">
+          </a>
+        </div>
+
+        <div class="menu-inner-shadow"></div>
+
+        <ul class="menu-inner py-1">
+          <!-- Dashboard -->
+          <!-- Layouts -->
+
+          <li class="menu-item ">
+
+            <a href="index.php" class="menu-link">
+
+              <i class="menu-icon tf-icons bx bx-home-circle"></i>
+
+              <div data-i18n="Analytics">Home</div>
+
+            </a>
+
+          </li>
+
+          <li class="menu-item ">
+
+            <a href="dashboard.php" class="menu-link">
+
+              <i class="menu-icon tf-icons bx bx-crown"></i>
+
+              <div data-i18n="Analytics">Dashbord</div>
+
+            </a>
+
+          </li>
+
+          <li class="menu-item ">
+
+            <a href="student_datails.php" class="menu-link">
+
+              <i class="menu-icon tf-icons bx bx-lock-open-alt"></i>
+
+              <div data-i18n="Analytics">Players</div>
+
+            </a>
+
+          </li>
+
+          <li class="menu-item ">
+
+            <a href="register.php" class="menu-link">
+
+              <i class="menu-icon tf-icons bx bx-user"></i>
+
+              <div data-i18n="Analytics">Registration</div>
+
+            </a>
+
+          </li>
+
+          <li class="menu-item ">
+
+            <a href="payment_conformation.php" class="menu-link">
+
+              <i class="menu-icon tf-icons bx bx-copy"></i>
+
+              <div data-i18n="Analytics">Payment Confirmation</div>
+
+            </a>
+
+          </li>
+
+          <li class="menu-item ">
+
+            <a href="game_confirmation.php" class="menu-link">
+
+              <i class="menu-icon tf-icons bx bx-support"></i>
+
+              <div data-i18n="Analytics">Game Confirmation</div>
+
+            </a>
+
+          </li>
+
+          <li class="menu-item ">
+
+            <a href="editstudent.php" class="menu-link">
+
+              <i class="menu-icon tf-icons bx bx-copy"></i>
+
+              <div data-i18n="Analytics">EDIT USER</div>
+
+            </a>
+
+          </li>
+
+
+          <li class="menu-item ">
+
+            <a href="certificate.php" class="menu-link">
+
+              <i class="menu-icon tf-icons bx bx-file"></i>
+
+              <div data-i18n="Analytics">Certificate</div>
+
+            </a>
+
+          </li>
+
+          <li class="menu-item ">
+
+            <a href="addword.php" class="menu-link">
+
+              <i class="menu-icon tf-icons bx bx-table"></i>
+
+              <div data-i18n="Analytics">Add New Words</div>
+
+            </a>
+
+          </li>
+
+
+          <li class="menu-item ">
+
+            <a href="replay.php" class="menu-link">
+
+              <i class="menu-icon tf-icons bx bx-dock-top"></i>
+
+              <div data-i18n="Analytics">Replay</div>
+
+            </a>
+
+          </li>
+
+
+          <li class="menu-item ">
+
+            <a href="logout.php" class="menu-link">
+
+              <i class="menu-icon tf-icons bx bx-cube-alt"></i>
+
+              <div data-i18n="Analytics">Logout</div>
+
+            </a>
+
+          </li>
+
+
+        </ul>
+      </aside>
+      <div class="layout-page">
+        <!-- Navbar -->
+
+        <nav class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme" id="layout-navbar">
+
+          <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
+
+            <div class="navbar-nav align-items-center">
+              <div class="nav-item d-flex align-items-center">
+                <i class="bx bx-search fs-4 lh-0"></i>
+                <input type="text" class="form-control border-0 shadow-none" id="searchInput" title="Search with regno or name or phone number....." placeholder="Search with regno or name or phone number....." aria-label="Search with regno or name or phone number....." />
+              </div>
+            </div>
+
+            <ul class="navbar-nav flex-row align-items-center ms-auto">
+              <!-- User -->
+              <li class="nav-item navbar-dropdown dropdown-user dropdown">
+                <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
+                  <div class="avatar avatar-online">
+                    <img src="Bhavani/img/team_1.jpg" alt class="w-px-40 h-auto rounded-circle" />
+                  </div>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end">
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      <div class="d-flex">
+                        <div class="flex-shrink-0 me-3">
+                          <div class="avatar avatar-online">
+                            <img src="Bhavani/img/team_1.jpg" alt class="w-px-40 h-auto rounded-circle" />
+                          </div>
+                        </div>
+                        <div class="flex-grow-1">
+                          <span class="fw-semibold d-block">Dr. Suresh Babu</span>
+                          <small class="text-muted">Founder BVRMOL</small>
+                        </div>
+                      </div>
+                    </a>
+                  </li>
+
+                  <li>
+                    <div class="dropdown-divider"></div>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="logout.php">
+                      <i class="bx bx-power-off me-2"></i>
+                      <span class="align-middle">Log Out</span>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <!--/ User -->
+            </ul>
+          </div>
+        </nav>
+
+        <!-- / Navbar -->
+
+        <div class="content-wrapper">
+        <?php } elseif ($_SESSION['category'] == 'superadmin') { ?>
+          <div class="layout-wrapper layout-content-navbar">
+            <div class="layout-container">
+
+              <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+                <div class="app-brand demo">
+                  <a href="../index.php" class="app-brand-link">
+                    <img src="Bhavani/img/red.png" alt="">
+                  </a>
+                </div>
+
+                <div class="menu-inner-shadow"></div>
+
+                <ul class="menu-inner py-1">
+                  <!-- Dashboard -->
+                  <!-- Layouts -->
+
+                  <li class="menu-item ">
+
+                    <a href="index.php" class="menu-link">
+
+                      <i class="menu-icon tf-icons bx bx-home-circle"></i>
+
+                      <div data-i18n="Analytics">Home</div>
+
+                    </a>
+
+                  </li>
+
+                  <li class="menu-item ">
+
+                    <a href="dashboard.php" class="menu-link">
+
+                      <i class="menu-icon tf-icons bx bx-crown"></i>
+
+                      <div data-i18n="Analytics">Dashbord</div>
+
+                    </a>
+
+                  </li>
+
+                  <li class="menu-item ">
+
+                    <a href="student_datails.php" class="menu-link">
+
+                      <i class="menu-icon tf-icons bx bx-lock-open-alt"></i>
+
+                      <div data-i18n="Analytics">Players</div>
+
+                    </a>
+
+                  </li>
+
+                  <li class="menu-item ">
+
+                    <a href="register.php" class="menu-link">
+
+                      <i class="menu-icon tf-icons bx bx-user"></i>
+
+                      <div data-i18n="Analytics">Registration</div>
+
+                    </a>
+
+                  </li>
+
+                  <li class="menu-item ">
+
+                    <a href="payment_conformation.php" class="menu-link">
+
+                      <i class="menu-icon tf-icons bx bx-copy"></i>
+
+                      <div data-i18n="Analytics">Payment Confirmation</div>
+
+                    </a>
+
+                  </li>
+
+                  <li class="menu-item ">
+
+                    <a href="game_confirmation.php" class="menu-link">
+
+                      <i class="menu-icon tf-icons bx bx-support"></i>
+
+                      <div data-i18n="Analytics">Game Confirmation</div>
+
+                    </a>
+
+                  </li>
+
+                  <li class="menu-item ">
+
+                    <a href="editstudent.php" class="menu-link">
+
+                      <i class="menu-icon tf-icons bx bx-copy"></i>
+
+                      <div data-i18n="Analytics">EDIT USER</div>
+
+                    </a>
+
+                  </li>
+
+                  <li class="menu-item ">
+
+                    <a href="addword.php" class="menu-link">
+
+                      <i class="menu-icon tf-icons bx bx-table"></i>
+
+                      <div data-i18n="Analytics">Add New Words</div>
+
+                    </a>
+
+                  </li>
+
+
+
+                  <li class="menu-item ">
+
+                    <a href="certificate.php" class="menu-link">
+
+                      <i class="menu-icon tf-icons bx bx-file"></i>
+
+                      <div data-i18n="Analytics">Certificate</div>
+
+                    </a>
+
+                  </li>
+
+
+
+                  <li class="menu-item ">
+
+                    <a href="replay.php" class="menu-link">
+
+                      <i class="menu-icon tf-icons bx bx-dock-top"></i>
+
+                      <div data-i18n="Analytics">Replay</div>
+
+                    </a>
+
+                  </li>
+
+
+                  <li class="menu-item ">
+
+                    <a href="logout.php" class="menu-link">
+
+                      <i class="menu-icon tf-icons bx bx-cube-alt"></i>
+
+                      <div data-i18n="Analytics">Logout</div>
+
+                    </a>
+
+                  </li>
+
+
+                </ul>
+              </aside>
+              <div class="layout-page">
+                <!-- Navbar -->
+
+                <nav class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme" id="layout-navbar">
+
+                  <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
+
+                    <div class="navbar-nav align-items-center">
+                      <div class="nav-item d-flex align-items-center">
+                        <i class="bx bx-search fs-4 lh-0"></i>
+                        <input type="text" class="form-control border-0 shadow-none" id="searchInput" title="Search with regno or name or phone number....." placeholder="Search with regno or name or phone number....." aria-label="Search with regno or name or phone number....." />
+                      </div>
+                    </div>
+
+                    <ul class="navbar-nav flex-row align-items-center ms-auto">
+                      <!-- User -->
+                      <li class="nav-item navbar-dropdown dropdown-user dropdown">
+                        <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
+                          <div class="avatar avatar-online">
+                            <img src="Bhavani/img/team_1.jpg" alt class="w-px-40 h-auto rounded-circle" />
+                          </div>
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end">
+                          <li>
+                            <a class="dropdown-item" href="#">
+                              <div class="d-flex">
+                                <div class="flex-shrink-0 me-3">
+                                  <div class="avatar avatar-online">
+                                    <img src="Bhavani/img/team_1.jpg" alt class="w-px-40 h-auto rounded-circle" />
+                                  </div>
+                                </div>
+                                <div class="flex-grow-1">
+                                  <span class="fw-semibold d-block">Dr. Suresh Babu</span>
+                                  <small class="text-muted">Founder BVRMOL</small>
+                                </div>
+                              </div>
+                            </a>
+                          </li>
+
+                          <li>
+                            <div class="dropdown-divider"></div>
+                          </li>
+                          <li>
+                            <a class="dropdown-item" href="logout.php">
+                              <i class="bx bx-power-off me-2"></i>
+                              <span class="align-middle">Log Out</span>
+                            </a>
+                          </li>
+                        </ul>
+                      </li>
+                      <!--/ User -->
+                    </ul>
+                  </div>
+                </nav>
+
+                <!-- / Navbar -->
+
+                <div class="content-wrapper">
+                <?php } else { ?>
+
+                  <!-- Sidebar Starts Here Shiva -->
+                  <div class="layout-wrapper layout-content-navbar">
+                    <div class="layout-container">
+
+                      <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+                        <div class="app-brand demo">
+                          <a href="../index.php" class="app-brand-link">
+                            <img src="Bhavani/img/red.png" alt="">
+                          </a>
+                        </div>
+
+                        <div class="menu-inner-shadow"></div>
+
+                        <ul class="menu-inner py-1">
+                          <!-- Dashboard -->
+
+                          <!-- Layouts -->
+                          <li class="menu-item ">
+                            <a href="students.php" class="menu-link">
+                              <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                              <div data-i18n="Analytics">Home</div>
+                            </a>
+                          </li>
+                          <li class="menu-item ">
+                            <a href="logout.php" class="menu-link">
+                              <i class="menu-icon tf-icons bx bx-cube-alt"></i>
+                              <div data-i18n="Analytics">Logout</div>
+                            </a>
+                          </li>
+                        </ul>
+                      </aside>
+                      <div class="layout-page">
+                        <!-- Navbar -->
+
+                        <nav class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme" id="layout-navbar">
+
+                          <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
+                            <div class="navbar-nav align-items-center">
+                              <div class="nav-item d-flex align-items-center">
+                                <i class="bx bx-search fs-4 lh-0"></i>
+                                <input type="text" class="form-control border-0 shadow-none" id="searchInput" title="Search with regno or name or phone number....." placeholder="Search with regno or name or phone number....." aria-label="Search with regno or name or phone number....." />
+                              </div>
+                            </div>
+
+
+                            <ul class="navbar-nav flex-row align-items-center ms-auto">
+                              <!-- User -->
+                              <li class="nav-item navbar-dropdown dropdown-user dropdown">
+                                <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
+                                  <div class="avatar avatar-online">
+                                    <img src="Bhavani/img/team_1.jpg" alt class="w-px-40 h-auto rounded-circle" />
+                                  </div>
+                                </a>
+                                <ul class="dropdown-menu dropdown-menu-end">
+                                  <li>
+                                    <a class="dropdown-item" href="#">
+                                      <div class="d-flex">
+                                        <div class="flex-shrink-0 me-3">
+                                          <div class="avatar avatar-online">
+                                            <img src="Bhavani/img/team_1.jpg" alt class="w-px-40 h-auto rounded-circle" />
+                                          </div>
+                                        </div>
+                                        <div class="flex-grow-1">
+                                          <span class="fw-semibold d-block">Dr. Suresh Babu</span>
+                                          <small class="text-muted">Founder BVRMOL</small>
+                                        </div>
+                                      </div>
+                                    </a>
+                                  </li>
+
+                                  <li>
+                                    <div class="dropdown-divider"></div>
+                                  </li>
+                                  <li>
+                                    <a class="dropdown-item" href="logout.php">
+                                      <i class="bx bx-power-off me-2"></i>
+                                      <span class="align-middle">Log Out</span>
+                                    </a>
+                                  </li>
+                                </ul>
+                              </li>
+                              <!--/ User -->
+                            </ul>
+                          </div>
+                        </nav>
+
+                        <!-- / Navbar -->
+
+                        <div class="content-wrapper">
+
+                          <!-- Sidebar Ends Here Shiva -->
+
+                        <?php } ?>
---
+++ admin/login1.php
@@ -0,0 +1,134 @@
+<?php
+session_start();
+include("connect.php");
+
+if (!empty($_SESSION['admin'])) {
+  header("location: index.php");
+}
+if (isset($_POST['submit'])) {
+  $admin = $_POST['admin'];
+  $pass = $_POST['pass'];
+  if (mysqli_num_rows(mysqli_query($conn, "SELECT * FROM `admins` WHERE `mobile`='$admin' AND `password`='$pass'")) == 1) {
+    $_SESSION['category'] = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM `admins` WHERE `mobile`='$admin' AND `password`='$pass'"))['category'];
+
+    $_SESSION['admin'] = $admin;
+    if($_SESSION['category'] == 'stall' || $_SESSION['category'] == 'superadmin')
+    header("location: index.php");
+    else
+    header("location: students.php");
+
+  } else {
+    echo "<script>alert('This is Not For You')</script>";
+  }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en" class="light-style customizer-hide" dir="ltr" data-theme="theme-default" data-assets-path="Bhavani/" data-template="vertical-menu-template-free">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+
+  <title>SpellBee SRKR Admin</title>
+
+  <meta name="description" content="" />
+
+  <!-- Favicon -->
+  <link rel="icon" type="image/x-icon" href="Bhavani/img/cup.png" />
+
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
+
+  <!-- Icons. Uncomment required icon fonts -->
+  <link rel="stylesheet" href="Bhavani/vendor/fonts/boxicons.css" />
+
+  <!-- Core CSS -->
+  <link rel="stylesheet" href="Bhavani/vendor/css/core.css" class="template-customizer-core-css" />
+  <link rel="stylesheet" href="Bhavani/vendor/css/theme-default.css" class="template-customizer-theme-css" />
+  <link rel="stylesheet" href="Bhavani/css/demo.css" />
+
+  <!-- Vendors CSS -->
+  <link rel="stylesheet" href="Bhavani/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+
+  <!-- Page CSS -->
+  <!-- Page -->
+  <link rel="stylesheet" href="Bhavani/vendor/css/pages/page-auth.css" />
+  <!-- Helpers -->
+  <script src="Bhavani/vendor/js/helpers.js"></script>
+
+  <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+  <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+  <script src="Bhavani/js/config.js"></script>
+</head>
+
+<body>
+  <!-- Content -->
+
+  <div class="container-xxl">
+    <div class="authentication-wrapper authentication-basic container-p-y">
+      <div class="authentication-inner">
+        <!-- Register -->
+        <div class="card">
+          <div class="card-body">
+            <!-- Logo -->
+            <div class="app-brand justify-content-center">
+              <a href="index.html" class="app-brand-link gap-2">
+                <span class="app-brand-text demo text-body fw-bolder">SPELLBEE ADMIN</span>
+              </a>
+            </div>
+            <!-- /Logo -->
+            <form id="formAuthentication" class="mb-3" action="#" method="POST">
+              <div class="mb-3 form-password-toggle">
+                <div class="d-flex justify-content-between">
+                  <label class="form-label" for="password">Admin Mobile Number</label>
+                </div>
+                <div class="input-group input-group-merge">
+                  <input type="text" id="password" class="form-control" autocomplete="off" name="admin" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="password" />
+                </div>
+                <div class="d-flex justify-content-between">
+                  <label class="form-label" for="password">Password</label>
+                </div>
+                <div class="input-group input-group-merge">
+                  <input type="password" id="password" class="form-control" autocomplete="off" name="pass" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="password" />
+                </div>
+              </div>
+              <div class="mb-3">
+                <button class="btn btn-primary d-grid w-100" name="submit" type="submit">Sign in</button>
+              </div>
+            </form>
+
+          </div>
+        </div>
+        <!-- /Register -->
+      </div>
+    </div>
+  </div>
+
+  <!-- / Content -->
+
+
+  <!-- Core JS -->
+  <!-- build:js assets/vendor/js/core.js -->
+  <script src="Bhavani/vendor/libs/jquery/jquery.js"></script>
+  <script src="Bhavani/vendor/libs/popper/popper.js"></script>
+  <script src="Bhavani/vendor/js/bootstrap.js"></script>
+  <script src="Bhavani/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+
+  <script src="Bhavani/vendor/js/menu.js"></script>
+  <!-- endbuild -->
+
+  <!-- Vendors JS -->
+
+  <!-- Main JS -->
+  <script src="Bhavani/js/main.js"></script>
+
+  <!-- Page JS -->
+
+  <!-- Place this tag in your head or just before your close body tag. -->
+  <script async defer src="https://buttons.github.io/buttons.js"></script>
+</body>
+
+</html>
